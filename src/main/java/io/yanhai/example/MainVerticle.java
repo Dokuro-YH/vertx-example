74f3a0d47d7a0d2afc36862491e7049cfa9e3fb4
yanhai <shimmly@live.cn>
2018-01-29 17:39:40 +0800

    add APIRouter.java

--- src/main/java/io/yanhai/example/MainVerticle.java
+++ src/main/java/io/yanhai/example/MainVerticle.java
@@ -15,16 +15,7 @@ import io.vertx.ext.auth.jdbc.JDBCAuth;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
-import io.vertx.ext.web.handler.CookieHandler;
-import io.vertx.ext.web.handler.SessionHandler;
-import io.vertx.ext.web.handler.UserSessionHandler;
-import io.vertx.ext.web.sstore.LocalSessionStore;
-import io.yanhai.example.common.RestResourceManagerRouter;
-import io.yanhai.example.user.jdbc.JdbcUserManager;
-import io.yanhai.example.web.auth.AuthorizeRequestHandler;
-import io.yanhai.example.web.login.JsonLoginHandler;
-import io.yanhai.example.web.login.LogoutHandler;
+import io.yanhai.example.web.api.APIRouter;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -144,23 +135,7 @@ public class MainVerticle extends AbstractVerticle {
 
     Router router = Router.router(vertx);
 
-    // router.route().handler(LoggerHandler.create());
-    router.route().handler(BodyHandler.create());
-    router.route().handler(CookieHandler.create());
-    // TODO SessionHandler 如果返回状态码不是 2xx or 3xx，会自动删除cookie（应该是出于安全考虑）
-    router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
-    router.route().handler(UserSessionHandler.create(authProvider));
-
-    // 登录授权
-    router.post("/login").handler(JsonLoginHandler.create(authProvider));
-    router.post("/logout").handler(LogoutHandler.create());
-
-    // 用户管理路由
-    Router userManagerRouter = RestResourceManagerRouter.router(vertx,
-        JdbcUserManager.create(authProvider, jdbcClient),
-        s -> s == null || s.isEmpty() ? null : new JsonObject(s));
-    router.route("/users").handler(AuthorizeRequestHandler.create("role:admin"));
-    router.mountSubRouter("/users", userManagerRouter);
+    router.mountSubRouter("/api", APIRouter.create(vertx, authProvider, jdbcClient));
 
     router.get().handler(req -> {
       final String msg;
@@ -179,5 +154,4 @@ public class MainVerticle extends AbstractVerticle {
 
     return fut;
   }
-
 }
